35559fc751eccbadf696072c4d47147a894deb67
harlamova0805 <nmharlamova2022@yandex.ru>
2023-05-09 12:11:53 +0300

    Исправленное решение задачи 3_2

--- homework3/task3_2.py
+++ homework3/task3_2.py
@@ -9,7 +9,8 @@ def get_list_info(lst):
     min_elem = min(lst)
     max_elem = max(lst)
     sum_list = sum(lst)
-    average = round(mean(lst), 2)
+    #  average = round(mean(lst), 2)
+    average = round(sum_list / 7, 2)  # Решение без mean, или average = round(sum(lst) / 7, 2)
     return min_elem, max_elem, sum_list, average
 
 
